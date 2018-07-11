ec6dbb37bb1db81c9f4e35500526585f8ce47bee
Jamie Feeny <feeny.jamie@gmail.com>
2018-07-11 12:07:10 -0700

    Updated with comments

--- LargestNumber/main.m
+++ LargestNumber/main.m
@@ -11,13 +11,13 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSArray *allNumbers = @[@864, @864, @7.12, @123, @3.14];
-        NSNumber *largestNum = @0;
-        for (NSNumber *comparedNum in allNumbers)
+        NSNumber *largestNum = @0; //set largest number to a value
+        for (NSNumber *comparedNum in allNumbers) //for each pointer in the array
         {
-            if ([comparedNum isGreaterThan:largestNum])
+            if ([comparedNum isGreaterThan:largestNum])//if the value of the object stored at each pointer in the array isGreaterThan the value of the object sorted at the pointer largestNum
             {
-                NSLog(@"%@ is larger than %@", comparedNum, largestNum );
-                largestNum = comparedNum;
+                NSLog(@"%@ is larger than %@", comparedNum, largestNum ); // test statement
+                largestNum = comparedNum; // change the pointer of NSNumber largestNum to the new pointer from comparedNum
                 
             }
             else
@@ -25,7 +25,7 @@ int main(int argc, const char * argv[]) {
                 NSLog(@"%@ is not larger than %@", comparedNum, largestNum );
             }
         }
-            NSLog(@"%@ is the largest number", largestNum );
+            NSLog(@"%@ is the largest number", largestNum ); // print the object stored at the pointer largestNum
         }
         return 0;
     }
